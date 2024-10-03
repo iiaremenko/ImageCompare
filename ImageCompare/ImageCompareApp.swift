87f5f1a8a835ba620354d54dbecae6f6bcb756e0
Iurii Iaremenko <iiaremenko@intellias.com>
2024-10-03 15:17:53 +0200

    Initial Commit

---
+++ ImageCompare/ImageCompareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCompareApp.swift
+//  ImageCompare
+//
+//  Created by iurii int on 03.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageCompareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageCompareTests/ImageCompareTests.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCompareTests.swift
+//  ImageCompareTests
+//
+//  Created by iurii int on 03.10.2024.
+//
+
+import Testing
+
+struct ImageCompareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
